0b610e0d5aa36eb0cc7251c000e3a7e6821b5bb2
SongAlfio <Song.Alfio@iisgalvanimi.edu.it>
2023-05-04 13:00:14 +0000

    Aggiornamento

--- angular-maps/src/app/app.component.ts
+++ angular-maps/src/app/app.component.ts
@@ -11,14 +11,29 @@ export class AppComponent {
   center : any;
   position : any;
   label :string;
+  markerOptions!: google.maps.MarkerOptions;
   circleOptions: any
   constructor()
   {
     this.center = {lat: 45.506738, lng: 9.190766};
     this.position = [this.center, {lat: 45.512971, lng: 9.13770}];
     this.label = "Sono qua";
-    this.circleOptions = {fillColor : 'red'}
+    this.circleOptions = {fillColor : 'red'};
 
+    let iconData : google.maps.Icon = {
+      url:'./assets/img/1200x630cw.ico',
+      scaledSize : new google.maps.Size(60,60)
+    }
+  
+    this.markerOptions = {icon:iconData}
   }
-
+  Giallo() {  
+    this.circleOptions = {fillColor : 'yellow'};
+  }  
+  Rosso() {  
+    this.circleOptions = {fillColor : 'red'};
+  }  
+  Blu() {  
+    this.circleOptions = {fillColor : 'blue'};
+  }  
 }
